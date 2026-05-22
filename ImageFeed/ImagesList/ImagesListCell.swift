c491209d53da887cda5d844ed13da74d73eda7bf
Sagida <sagida.karzhaubayeva@gmail.com>
2026-05-22 19:26:51 +0500

    add table view and custom cell

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+}
