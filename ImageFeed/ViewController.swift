8dd752c95adf9ddf6ebc3979bbb3590f331f4b92
Sagida <sagida.karzhaubayeva@gmail.com>
2026-05-25 15:19:25 +0500

    Revert eb79aeb, dd6cdc2, c 491209

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Sagida on 15.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
