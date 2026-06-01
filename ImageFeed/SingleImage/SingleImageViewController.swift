b6088edd93025b78caebd61268d9505871fb9a3a
Sagida <sagida.karzhaubayeva@gmail.com>
2026-06-01 12:58:30 +0500

    feat: сверстать экран для показа одного изображения, настроить переход к SingleImageViewController

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Sagida on 01.06.2026.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
