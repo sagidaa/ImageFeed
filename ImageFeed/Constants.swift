6a2542915580001147a3bb9a93acb62f0242cdce
Sagida <sagida.karzhaubayeva@gmail.com>
2026-06-15 15:48:28 +0500

    feat: добавить файл Constants с константами для Unsplash API

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Sagida on 11.06.2026.
+//
+
+import UIKit
+
+enum Constants {
+    static let accessKey = "cg4grDs2lwADWkZyk2dL78q7DlZAFnipxAfyIK9tBdY"
+    static let secretKey = "75yX_ib4FETkEBD_X9wkNvZSkNwcTozPwIcSohs4DKY"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURLString = "https://api.unsplash.com"
+}
