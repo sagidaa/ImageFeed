17ccd8f5b11621836425ef8a705f0ac25d61e09e
Sagida <sagida.karzhaubayeva@gmail.com>
2026-06-15 16:52:41 +0500

    feat: добавить WebViewViewController с WKWebView и переходом по нажатию кнопки "Войти"

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -8,5 +8,6 @@
 import UIKit
 
 final class AuthViewController: UIViewController {
-    
+    private let showWebViewSegueIdentifier = "ShowWebView"
+
 }
---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Sagida on 15.06.2026.
+//
+
+import WebKit
+
+final class WebViewViewController: UIViewController {
+    
+    @IBOutlet private var webView: WKWebView!
+}
