3d90fa202fbdc90caacf85ef7c825d14c3dc28ee
Sagida <sagida.karzhaubayeva@gmail.com>
2026-06-16 15:05:37 +0500

    feat: добавить загрузку экрана авторизации Unsplash через WKWebView

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -9,12 +9,17 @@ import UIKit
 import WebKit
 
 final class WebViewViewController: UIViewController {
+    
+    enum WebViewConstants {
+        static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+    }
         
     private let webView =  WKWebView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupWebView()
+        loadAuthView()
     }
     
     private func setupWebView() {
@@ -28,5 +33,24 @@ final class WebViewViewController: UIViewController {
             webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+    }
+    
+    private func loadAuthView() {
+        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else {
+            print("Error: failed to create URLComponents from string")
+            return
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope)
+        ]
+        
+        guard let url = urlComponents.url else { return }
+        print("Error: failed to construct URL from components")
+        let request = URLRequest(url: url)
+        webView.load(request)
     }
 }
