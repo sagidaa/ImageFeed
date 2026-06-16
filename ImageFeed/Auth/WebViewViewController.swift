d247147b2f857cf23e2e77cc26b29bfb9e412085
Sagida <sagida.karzhaubayeva@gmail.com>
2026-06-16 13:35:40 +0500

    refactor: переверстать WKWebView programmatically

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -5,9 +5,28 @@
 //  Created by Sagida on 15.06.2026.
 //
 
+import UIKit
 import WebKit
 
 final class WebViewViewController: UIViewController {
+        
+    private let webView =  WKWebView()
     
-    @IBOutlet private var webView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupWebView()
+    }
+    
+    private func setupWebView() {
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.backgroundColor = .ypWhite
+        view.addSubview(webView)
+        
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
