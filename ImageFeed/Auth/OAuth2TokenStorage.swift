12f400907c7462a215bd6b29f85850f0dc3f70e8
Sagida <sagida.karzhaubayeva@gmail.com>
2026-06-26 13:03:55 +0500

    feat: добавить хранилище OAuth-токена

---
+++ ImageFeed/Auth/OAuth2TokenStorage.swift
@@ -0,0 +1,21 @@
+//
+//  OAuth2TokenStorage.swift
+//  ImageFeed
+//
+//  Created by Sagida on 25.06.2026.
+//
+
+import Foundation
+
+final class OAuth2TokenStorage {
+    private let storage : UserDefaults = .standard
+    
+    private enum Keys: String {
+        case token
+    }
+    
+    var token: String? {
+        get { storage.string(forKey: Keys.token.rawValue) }
+        set { storage.set(newValue, forKey: Keys.token.rawValue) }
+    }
+}
