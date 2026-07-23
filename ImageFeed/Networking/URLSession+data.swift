3b95be3e2c06072ad1d201d015644c888bd042c3
Sagida <sagida.karzhaubayeva@gmail.com>
2026-07-23 17:22:51 +0500

    feat: добавить generic-метод objectTask для декодирования ответа сервера

--- ImageFeed/Networking/URLSession+data.swift
+++ ImageFeed/Networking/URLSession+data.swift
@@ -50,3 +50,30 @@ extension URLSession {
         return task
     }
 }
+
+extension URLSession {
+    func objectTask<T: Decodable>(
+        for request: URLRequest,
+        completion: @escaping (Result<T, Error>) -> Void
+    ) -> URLSessionTask {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let task = data(for: request) { (result: Result<Data, Error>) in
+            switch result {
+            case .success(let data):
+                do {
+                    let decodedObject = try decoder.decode(T.self, from: data)
+                    completion(.success(decodedObject))
+                } catch {
+                    print("Decoding error: \(error)")
+                    completion(.failure(NetworkError.decodingError(error)))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+        
+        return task
+    }
+}
