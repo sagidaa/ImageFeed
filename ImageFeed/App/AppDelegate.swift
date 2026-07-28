cf59a3cd4fd32e540889c76c439271727bb91c31
Sagida <sagida.karzhaubayeva@gmail.com>
2026-07-28 14:32:24 +0500

    refactor: перенести настройку UIWindow в SceneDelegate

--- ImageFeed/App/AppDelegate.swift
+++ ImageFeed/App/AppDelegate.swift
@@ -10,18 +10,24 @@ import UIKit
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        let sceneConfiguration = UISceneConfiguration(
+            name: "Main",
+            sessionRole: connectingSceneSession.role
+        )
+        
+        sceneConfiguration.delegateClass = SceneDelegate.self
+        return sceneConfiguration
     }
-
-
 }
 
