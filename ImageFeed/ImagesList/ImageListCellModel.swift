8125fe2cce6c54b5e004580aed4e1ac097a20fed
Sagida <sagida.karzhaubayeva@gmail.com>
2026-07-29 14:17:48 +0500

    refactor: переверстать ImageListCell programmatically

---
+++ ImageFeed/ImagesList/ImageListCellModel.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  ImageFeed
+//
+//  Created by Sagida on 28.07.2026.
+//
+
+import Foundation
+
+struct ImageListCellModel {
+    let photo: String
+    let date: String
+    let isLiked: Bool
+}
