6a523fb9847b26f91dfb4bb2860ca322daa121ab
Sagida <sagida.karzhaubayeva@gmail.com>
2026-07-31 14:14:07 +0500

    refactor: завершить отказ от Main.storyboard

--- ImageFeed/App/SceneDelegate.swift
+++ ImageFeed/App/SceneDelegate.swift
@@ -12,7 +12,7 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let scene = (scene as? UIWindowScene) else { return }
+        guard let scene = scene as? UIWindowScene else { return }
         window = UIWindow(windowScene: scene)
         window?.rootViewController = SplashViewController()
         window?.makeKeyAndVisible()
